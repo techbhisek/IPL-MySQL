972a128ca3cfc56bc4c1e5bba72341aa74fb41e3
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-12-02 14:06:35 +0530

    optimize the by using Case

--- Queries/bestEconomyInSuperOver.sql
+++ Queries/bestEconomyInSuperOver.sql
@@ -9,13 +9,18 @@ FROM
         is_super_over != 0
     GROUP BY bowler) t1
         JOIN
-    (SELECT 
-        bowler, COUNT(*) / 6 AS overs
-    FROM
-        Deliveries
-    WHERE
-        is_super_over != 0 && noball_runs = 0
-            && wide_runs = 0
-    GROUP BY bowler) t2 ON t1.bowler = t2.bowler
-ORDER BY runs / overs
-LIMIT 1;
+select * from(
+SELECT 
+  Rank() over(order by SUM(batsman_runs + noball_runs + wide_runs) * 6 / COUNT(CASE
+        WHEN noball_runs = 0 AND wide_runs = 0 THEN bowler
+        ELSE NULL
+    END)) position,bowler,
+SUM(batsman_runs + noball_runs + wide_runs) * 6 / COUNT(CASE
+        WHEN noball_runs = 0 AND wide_runs = 0 THEN bowler
+        ELSE NULL
+    END) AS economy
+FROM
+    Deliveries
+WHERE
+    is_super_over != 0
+GROUP BY bowler) t1 where position=1
