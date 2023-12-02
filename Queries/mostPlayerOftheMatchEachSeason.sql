d42945e6c17297e3b8fb49fb66b511da6659121e
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-12-02 14:06:53 +0530

    optimize by using the rank

--- Queries/mostPlayerOftheMatchEachSeason.sql
+++ Queries/mostPlayerOftheMatchEachSeason.sql
@@ -1,28 +1,11 @@
-SELECT 
-    datain.season, player, count
-FROM
-    ((SELECT 
-        season, MAX(count) AS maxcount
-    FROM
-        (SELECT 
-        season, player_of_match, COUNT(player_of_match) AS count
-    FROM
-        Matches
-    WHERE
-        player_of_match != ''
-    GROUP BY season , player_of_match
-    ORDER BY season , COUNT(player_of_match) DESC) d1
-    GROUP BY season) datain
-    JOIN (SELECT 
-        season,
-            player_of_match AS player,
-            COUNT(player_of_match) AS count
-    FROM
-        Matches
-    WHERE
-        player_of_match != ''
-    GROUP BY season , player_of_match
-    ORDER BY season , COUNT(player_of_match) DESC) data2 ON datain.season = data2.season
-        AND datain.maxcount = data2.count);
-
-
+SELECT season, player_of_match, times FROM(
+   SELECT season,player_of_match ,count(player_of_match) AS times , 
+		Rank() OVER(PARTITION BY season ORDER BY count(*) DESC)  position 
+		FROM 
+			Matches
+		WHERE
+			player_of_match!=""
+		GROUP BY 
+			season,player_of_match) t1 
+WHERE
+ position=1;
