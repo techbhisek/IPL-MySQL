566cdb4235b69a6bc1a1d1a240ee3771558fbd6f
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-12-02 14:07:09 +0530

    optimized by using the case

--- Queries/top10EconomicalBowlersin2015.sql
+++ Queries/top10EconomicalBowlersin2015.sql
@@ -1,24 +1,13 @@
 SELECT 
-    bowler, run / overs
+    bowler,
+   SUM(batsman_runs + noball_runs + wide_runs)*6/
+    COUNT(CASE
+        WHEN noball_runs = 0 AND wide_runs = 0 THEN bowler
+        ELSE null
+    END) AS Economy
 FROM
-    (SELECT 
-        bowler, SUM(batsman_runs + wide_runs + noball_runs) AS run
-    FROM
-        Matches
-    JOIN Deliveries ON Matches.id = Deliveries.Match_id
-        AND Matches.season = 2015
-    GROUP BY bowler
-    ORDER BY SUM(batsman_runs + wide_runs + noball_runs)) d1
+    Matches
         JOIN
-    (SELECT 
-        bowler AS bowler1, COUNT(*) / 6 AS overs
-    FROM
-        Matches
-    JOIN Deliveries ON Matches.id = Deliveries.Match_id
+    Deliveries ON Matches.id = Deliveries.Match_id
         AND Matches.season = 2015
-        AND Deliveries.noball_runs = 0
-        AND Deliveries.wide_runs = 0
-    GROUP BY bowler
-    ORDER BY SUM(batsman_runs + wide_runs + noball_runs)) d2 ON d1.bowler = d2.bowler1
-ORDER BY run / overs
-LIMIT 10;
+GROUP BY bowler order by Economy Limit 10;
